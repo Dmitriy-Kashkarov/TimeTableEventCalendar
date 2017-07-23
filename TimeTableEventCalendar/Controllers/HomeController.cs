2ad656d304e5a8ccc35362825aa1cd8089f287be
Dmitriy Kashkarov <dimakashkarov@gmail.com>
2017-07-23 20:20:14 +0300

    Create event model. Add FullCalendar library in project. Create controller, main view

---
+++ TimeTableEventCalendar/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTableEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetEvents()
+        {
+            using (EventsDatabaseEntities db = new EventsDatabaseEntities())
+            {
+                var events = db.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
