2daed76e54d28f6e7389041c96b152ebd9825e65
Dmitriy Kashkarov <dimakashkarov@gmail.com>
2017-08-10 14:49:18 +0300

    Add CRUD options in project. Add CRUD logic in project.

--- TimeTableEventCalendar/Controllers/HomeController.cs
+++ TimeTableEventCalendar/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace TimeTableEventCalendar.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
@@ -23,5 +22,58 @@ namespace TimeTableEventCalendar.Controllers
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
         }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+
+            using (EventsDatabaseEntities db = new EventsDatabaseEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    var v = db.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                    db.Events.Add(e);
+
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+
+            using (EventsDatabaseEntities db = new EventsDatabaseEntities())
+            {
+                var v = db.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    db.Events.Remove(v);
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+
+
     }
 }
